75085ddc9b039331955c335aaa487eb84326945f
Divran <arviddivran@gmail.com>
2019-08-13 19:57:38 +0200

    updated IT3 tools

--- data/tools_it3.js
+++ data/tools_it3.js
@@ -1,564 +1,520 @@
 data.add("tools","it3",[
-
-// IT3 custom materials
 {
-	"age":0,
-	"tier":2,
 	"enchant":"",
+	"age":0,
+	"othermod":"",
 	"material":"Compressed wrought iron",
-	"othermod":"",
-	"speed":7.0,
 	"soft":0,
-	"durability":4600
+	"recommended":"Good early-game material.<small>Is a direct upgrade to wrought iron, if you go into the optional pneumaticraft side-quest path.</small>",
+	"durability":4600,
+	"speed":9.5,
+	"tier":2
 },
 {
-	"age":0,
-	"tier":2,
 	"enchant":"",
+	"age":0,
+	"othermod":"",
 	"material":"Compressed iron",
-	"othermod":"",
-	"speed":6.0,
 	"soft":0,
-	"durability":4350
+	"durability":4350,
+	"speed":6.5,
+	"tier":2
 },
 {
-	"age":0,
-	"tier":3,
 	"enchant":"",
+	"age":0,
+	"othermod":"",
 	"material":"Enderium",
-	"othermod":"",
-	"speed":8.0,
 	"soft":0,
-	"durability":2560
+	"durability":2560,
+	"speed":8.0,
+	"tier":3
 },
 {
-	"age":0,
-	"tier":2,
 	"enchant":"",
+	"age":0,
+	"othermod":"",
 	"material":"Signalum",
-	"othermod":"",
+	"soft":0,
+	"durability":1280,
 	"speed":5.0,
-	"soft":0,
-	"durability":1280
+	"tier":2
 },
 {
-	"age":0,
-	"tier":2,
 	"enchant":"",
+	"age":0,
+	"othermod":"",
 	"material":"Thaumium",
-	"othermod":"",
-	"speed":10,
 	"soft":0,
+	"recommended":"Excellent early-game material if you do some thaumcraft research.",
 	"durability":3150,
-	"recommended":"Excellent early-game material if you do some thaumcraft research."
-},
-
-
-// GTCE materials
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
 	"speed":10.0,
-	"durability":1280,
+	"tier":3
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
 	"material":"Aluminium",
-	"tier":2,
-	"othermod":""
+	"soft":0,
+	"durability":1280,
+	"speed":10.0,
+	"tier":2
 },
 {
-	"age":0,
 	"enchant":"",
-	"soft":0,
-	"speed":12.0,
-	"durability":5120,
+	"age":0,
+	"othermod":"",
 	"material":"Chrome",
-	"tier":3,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
 	"soft":0,
-	"speed":10.0,
-	"durability":2560,
-	"material":"Cobalt",
-	"tier":3,
-	"othermod":"",
-	"recommended":"Good early-game material<br><small>Has low durability, but very high mining speed.</small>"
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":7.0,
-	"durability":25600,
-	"material":"Iridium",
-	"tier":3,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"Sharpness I",
-	"soft":0,
-	"speed":7.0,
-	"durability":2560,
-	"material":"Iron",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":7.0,
 	"durability":5120,
-	"material":"Manganese",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":7.0,
-	"durability":5120,
-	"material":"Molybdenum",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":7.0,
-	"durability":5120,
-	"material":"Neodymium",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":24.0,
-	"durability":1553600,
-	"material":"Darmstadtium",
-	"tier":6,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":16.0,
-	"durability":12800,
-	"material":"Osmium",
-	"tier":4,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":10.0,
-	"durability":640,
-	"material":"Silver",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":6.0,
-	"durability":5120,
-	"material":"Thorium",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":7.0,
-	"durability":16000,
-	"material":"Titanium",
-	"tier":3,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":7.0,
-	"durability":25600,
-	"material":"Tungsten",
-	"tier":3,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":6.0,
-	"durability":5120,
-	"material":"Uranium",
-	"tier":3,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":6.0,
-	"durability":5120,
-	"material":"Uranium235",
-	"tier":3,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"Sharpness II",
-	"soft":0,
-	"speed":7.0,
-	"durability":1280,
-	"material":"Brass",
-	"tier":1,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"Sharpness I",
-	"soft":0,
-	"speed":6.0,
-	"durability":1920,
-	"material":"Bronze",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"Bane of Arthropods III",
-	"soft":0,
-	"speed":6.0,
-	"durability":2560,
-	"material":"Invar",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":6.0,
-	"durability":2560,
-	"material":"Magnalium",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":6.0,
-	"durability":3840,
-	"material":"Pig iron",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"Sharpness III",
-	"soft":0,
-	"speed":7.0,
-	"durability":4800,
-	"material":"Stainless steel",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"Sharpness II",
-	"soft":0,
-	"speed":6.0,
-	"durability":5120,
-	"material":"Steel",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":6.5,
-	"durability":960,
-	"material":"Tin alloy",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":9.0,
-	"durability":20480,
-	"material":"Ultimet",
-	"tier":4,
-	"othermod":"",
-	"recommended":"All around excellent mid-game material."
-},
-{
-	"age":0,
-	"enchant":"Sharpness II",
-	"soft":0,
-	"speed":6.0,
-	"durability":3840,
-	"material":"Wrought iron",
-	"tier":2,
-	"othermod":"",
-	"recommended":"Good early-game material<br><small>If you ever make iron tools - don't. Upgrade it to wrought iron first!</small>"
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":7.0,
-	"durability":16000,
-	"material":"Osmiridium",
-	"tier":3,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":13.0,
-	"durability":1280,
-	"material":"Sterling silver",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"Smite IV",
-	"soft":0,
-	"speed":14.0,
-	"durability":1280,
-	"material":"Rose gold",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"Smite II",
-	"soft":0,
 	"speed":12.0,
-	"durability":2560,
-	"material":"Black bronze",
-	"tier":2,
-	"othermod":""
+	"tier":3
 },
 {
-	"age":0,
-	"enchant":"Bane of Arthropods V",
-	"soft":0,
-	"speed":8.0,
-	"durability":2560,
-	"material":"Bismuth bronze",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"Sharpness IV",
-	"soft":0,
-	"speed":6.5,
-	"durability":7680,
-	"material":"Black steel",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"Sharpness IV",
-	"soft":0,
-	"speed":7.0,
-	"durability":8960,
-	"material":"Red steel",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"Sharpness V",
-	"soft":0,
-	"speed":7.5,
-	"durability":10240,
-	"material":"Blue steel",
-	"tier":2,
-	"othermod":"",
-	"recommended":"Great mid-game material<br><small>This material can be made as soon as you have your first electric blast furnace."
-},
-{
-	"age":0,
-	"enchant":"Sharpness V",
-	"soft":0,
-	"speed":8.0,
-	"durability":12800,
-	"material":"Damascus steel",
-	"tier":2,
-	"othermod":"",
-	"recommended":"Best early-game material.<br><small>Available as loot in villages, with high durability, fast mining speed.</small>"
-},
-{
-	"age":0,
 	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Cobalt",
 	"soft":0,
-	"speed":8.0,
+	"recommended":"Good early-game material<br><small>Has low durability, but very high mining speed.</small>",
+	"durability":2560,
+	"speed":10.0,
+	"tier":3
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Iridium",
+	"soft":0,
 	"durability":25600,
-	"material":"Tungsten steel",
-	"tier":4,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":8.0,
-	"durability":2560,
-	"material":"Cobalt brass",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":6.0,
-	"durability":2560,
-	"material":"Iron magnetic",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":6.0,
-	"durability":5120,
-	"material":"Steel magnetic",
-	"tier":2,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
 	"speed":7.0,
-	"durability":5120,
-	"material":"Neodymium magnetic",
-	"tier":2,
-	"othermod":""
+	"tier":3
 },
 {
-	"age":0,
-	"enchant":"Sharpness V",
-	"soft":0,
-	"speed":14.0,
-	"durability":12800,
-	"material":"Tungsten carbide",
-	"tier":4,
-	"othermod":""
-},
-{
-	"age":0,
 	"enchant":"",
-	"soft":0,
-	"speed":3.0,
-	"durability":19200,
-	"material":"Vanadium steel",
-	"tier":3,
-	"othermod":""
-},
-{
 	"age":0,
-	"enchant":"Sharpness IV",
-	"soft":0,
-	"speed":10.0,
-	"durability":40000,
-	"material":"HSS-G",
-	"tier":3,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"Sharpness V",
-	"soft":0,
-	"speed":10.0,
-	"durability":51200,
-	"material":"HSS-E",
-	"tier":4,
 	"othermod":"",
-	"recommended":"Best non-fusion material in the game.<br><small>Is a direct upgrade to tungsten steel tools.</small>"
-},
-{
-	"age":0,
-	"enchant":"Sharpness V",
+	"material":"Iron",
 	"soft":0,
-	"speed":14.0,
-	"durability":30000,
-	"material":"HSS-S",
-	"tier":4,
-	"othermod":""
+	"durability":2560,
+	"speed":7.0,
+	"tier":2
 },
 {
-	"age":0,
 	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Manganese",
 	"soft":0,
-	"speed":6.0,
+	"durability":5120,
+	"speed":7.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Molybdenum",
+	"soft":0,
+	"durability":5120,
+	"speed":7.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Neodymium",
+	"soft":0,
+	"durability":5120,
+	"speed":7.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Darmstadtium",
+	"soft":0,
+	"durability":1553600,
+	"speed":24.0,
+	"tier":6
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Osmium",
+	"soft":0,
 	"durability":12800,
-	"material":"Naquadah",
-	"tier":4,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":8.0,
-	"durability":51200,
-	"material":"Naquadah alloy",
-	"tier":5,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":6.0,
-	"durability":12800,
-	"material":"Naquadah enriched",
-	"tier":4,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
-	"speed":20.0,
-	"durability":102400,
-	"material":"Tritanium",
-	"tier":6,
-	"othermod":""
-},
-{
-	"age":0,
-	"enchant":"",
-	"soft":0,
 	"speed":16.0,
+	"tier":4
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Palladium",
+	"soft":0,
+	"durability":5120,
+	"speed":8.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Thorium",
+	"soft":0,
+	"durability":5120,
+	"speed":6.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Titanium",
+	"soft":0,
+	"durability":16000,
+	"speed":7.0,
+	"tier":3
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Tungsten",
+	"soft":0,
+	"durability":25600,
+	"speed":7.0,
+	"tier":3
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Uranium",
+	"soft":0,
+	"durability":5120,
+	"speed":6.0,
+	"tier":3
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Uranium235",
+	"soft":0,
+	"durability":5120,
+	"speed":6.0,
+	"tier":3
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Brass",
+	"soft":0,
+	"durability":1520,
+	"speed":8.0,
+	"tier":1
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Bronze",
+	"soft":0,
+	"durability":1920,
+	"speed":6.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Invar",
+	"soft":0,
+	"durability":5120,
+	"speed":7.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Magnalium",
+	"soft":0,
+	"durability":2560,
+	"speed":6.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Pig iron",
+	"soft":0,
+	"durability":3840,
+	"speed":6.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Stainless steel",
+	"soft":0,
+	"durability":4800,
+	"speed":7.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Steel",
+	"soft":0,
+	"durability":5120,
+	"speed":6.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Ultimet",
+	"soft":0,
+	"recommended":"All around excellent mid-game material.",
+	"durability":20480,
+	"speed":9.0,
+	"tier":4
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Wrought iron",
+	"soft":0,
+	"recommended":"Good early-game material<br><small>If you ever make iron tools - don't. Upgrade it to wrought iron first!</small>",
+	"durability":3840,
+	"speed":6.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Osmiridium",
+	"soft":0,
+	"durability":31520,
+	"speed":9.0,
+	"tier":3
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Sterling silver",
+	"soft":0,
+	"durability":1960,
+	"speed":13.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Rose gold",
+	"soft":0,
+	"durability":1520,
+	"speed":14.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Black bronze",
+	"soft":0,
+	"durability":2560,
+	"speed":12.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Bismuth bronze",
+	"soft":0,
+	"durability":2560,
+	"speed":8.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Black steel",
+	"soft":0,
+	"durability":7680,
+	"speed":6.5,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Red steel",
+	"soft":0,
+	"durability":8960,
+	"speed":7.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Blue steel",
+	"soft":0,
+	"recommended":"Great mid-game material<br><small>This material can be made as soon as you have your first electric blast furnace.",
+	"durability":10240,
+	"speed":7.5,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Damascus steel",
+	"soft":0,
+	"recommended":"Best early-game material.<br><small>Available as loot in villages, with high durability, fast mining speed.</small>",
+	"durability":12800,
+	"speed":8.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Tungsten steel",
+	"soft":0,
+	"durability":25600,
+	"speed":8.0,
+	"tier":4
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Cobalt brass",
+	"soft":0,
+	"durability":2560,
+	"speed":8.0,
+	"tier":2
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Tungsten carbide",
+	"soft":0,
+	"durability":12800,
+	"speed":12.0,
+	"tier":4
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Vanadium steel",
+	"soft":0,
+	"durability":19200,
+	"speed":7.0,
+	"tier":3
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"HSS-G",
+	"soft":0,
+	"durability":40000,
+	"speed":10.0,
+	"tier":3
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"HSS-E",
+	"soft":0,
+	"recommended":"Best non-fusion material in the game.<br><small>Is a direct upgrade to tungsten steel tools.</small>",
 	"durability":51200,
+	"speed":10.0,
+	"tier":4
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"HSS-S",
+	"soft":0,
+	"durability":30000,
+	"speed":15.0,
+	"tier":4
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Naquadah",
+	"soft":0,
+	"durability":12800,
+	"speed":6.0,
+	"tier":4
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Naquadah alloy",
+	"soft":0,
+	"durability":51200,
+	"speed":8.0,
+	"tier":5
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Naquadah enriched",
+	"soft":0,
+	"durability":12800,
+	"speed":6.0,
+	"tier":4
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
+	"material":"Tritanium",
+	"soft":0,
+	"durability":102400,
+	"speed":20.0,
+	"tier":6
+},
+{
+	"enchant":"",
+	"age":0,
+	"othermod":"",
 	"material":"Duranium",
-	"tier":5,
-	"othermod":""
+	"soft":0,
+	"durability":51200,
+	"speed":16.0,
+	"tier":5
 }
 ]);
