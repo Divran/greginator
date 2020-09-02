d284f89669adcd893ba72f70ae9a0fee5c6d4233
Divran <arviddivran@gmail.com>
2020-09-02 23:58:19 +0200

    added wanted production

--- js/greginator/overclock-calculator.js
+++ js/greginator/overclock-calculator.js
@@ -24,6 +24,7 @@ onVersionChanged(function(version) {
 	var results_elem = $("#gt-overclock-results",card);
 	var time_elem = $("#gt-overclock-time",card);
 	var results = $("#gt-overclock-results",card);
+	var wanted_elem = $("#gt-overclock-wanted",card);
 
 	function getTier(voltage) {
 		if (voltage <= 8) {return 1;}
@@ -36,10 +37,12 @@ onVersionChanged(function(version) {
 
 		var energy = parseInt(energy_elem.val());
 		var amps = parseInt(amps_elem.val());
-		var target = parseInt($("[name='gt-overclock-target']:checked",card).val());
+		var target_elem = $("[name='gt-overclock-target']:checked",card);
+		var target = parseInt(target_elem.val());
 		var output = parseInt(output_elem.val());
 		var time = parseInt(time_elem.val());
 		var input = parseInt(input_elem.val());
+		var wanted = parseInt(wanted_elem.val());
 
 		if (isNaN(energy) || isNaN(amps) || isNaN(target) || isNaN(output) || isNaN(time) || isNaN(input)) {
 			results.text("One or more of your inputs is an invalid number.");
@@ -59,6 +62,7 @@ onVersionChanged(function(version) {
 		var speed = Math.pow(2,overclocks);
 		time = time / speed;
 		var output_per_sec = output/time;
+		var input_per_sec = input/time;
 
 		function round3(n) {
 			return Math.round(n*1000)/1000;
@@ -74,15 +78,54 @@ onVersionChanged(function(version) {
 		txt.push("Energy consumption: <strong>" + energy + "</strong> eu/t" + amps_txt+".");
 		txt.push("Production: <strong>" + output + "</strong> every <strong>" + time + "</strong> second(s) " + 
 			"for a total of <strong>" + round3(output_per_sec) + "</strong> per second." );
-		txt.push("Consumption: <strong>" + round3(input/time) + "</strong> per second.");
+		txt.push("Consumption: <strong>" + round3(input_per_sec) + "</strong> per second.");
 
-		var processing_array = "<p>In a processing array, uses <strong>" + (energy*amps*16) + "</strong> eu/t, produces <strong>" + round3(output_per_sec*16) + "</strong>/s, "+
-				" and consumes <strong>" + round3(input/time*16) + "</strong>/s.</p>";
+		var processing_array = "<p>In a processing array, uses <strong>" + round3(energy*amps*16) + "</strong> eu/t, produces <strong>" + round3(output_per_sec*16) + "</strong> items/s, "+
+				" and consumes <strong>" + round3(input_per_sec*16) + "</strong> items/s.</p>";
 
-		results.html("<h5>Results</h5><p>"+txt.join("<br>")+"</p>"+processing_array);
+		var wanted_str = "";
+		if (!isNaN(wanted)) {
+			wanted_str += "<hr>";
+			if (wanted < output_per_sec) {
+				wanted_str += "A single machine is enough to keep up with " + wanted + "/s.";
+			} else {
+				var wanted_machines = wanted/output_per_sec;
+				wanted_str += "To produce <strong>" + wanted + "</strong> items/s, ";
+				wanted_str += "you need <strong>" + Math.ceil(wanted_machines) + "</strong>";
+
+				if (round3(wanted_machines) != Math.ceil(wanted_machines)) {
+					wanted_str += " <small>(" + round3(wanted_machines) + ")</small>";
+				}
+
+				wanted_str += " <strong>" + target_elem.attr("data-voltage") + "</strong> machines to keep up";
+
+				var wanted_arrays = wanted_machines < 16 ? 0 : Math.ceil(wanted_machines/16);
+				if (wanted_arrays > 0) {
+					wanted_str += ", or <strong>" + Math.ceil(wanted_machines/16) + "</strong>";
+					if (round3(wanted_machines/16) != Math.ceil(wanted_machines/16)) {
+						wanted_str += " <small>("+round3(wanted_machines/16)+")</small> processing arrays.";
+					}
+				} else {
+					wanted_str += ".";
+				}
+
+				wanted_str += "<br>These will consume <strong>" + round3(energy*amps*wanted_machines) + "</strong> eu/t and <strong>"+
+							 round3(input_per_sec*wanted_machines)+"</strong> items/s.";
+
+				if (wanted_arrays > 0) {
+					var wanted_arrays = 
+					wanted_str += " In processing arrays, consumes <strong>"+round3(energy*amps*wanted_arrays*16)+"</strong> eu/t"+
+									" and <strong>"+round3(input_per_sec*wanted_arrays)+"</strong> items/s.";
+				}
+			}
+
+			wanted_str = "<p>"+wanted_str+"</p>";
+		}
+
+		results.html("<h5>Results</h5><p>"+txt.join("<br>")+"</p>"+processing_array+wanted_str);
 	}
 
-	$([energy_elem[0],amps_elem[0],output_elem[0],time_elem[0]]).click(function() {
+	$([energy_elem[0],amps_elem[0],output_elem[0],time_elem[0],wanted_elem[0]]).click(function() {
 		$(this).select();
 	}).on("input",doCalc);
 	amps_buttons.click(function() {
