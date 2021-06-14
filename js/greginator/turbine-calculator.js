2024f23e7ebd5328e3b2ecc906beb607b152a6d3
Divran <arviddivran@gmail.com>
2021-06-15 00:37:01 +0200

    fixed time until destroyed

--- js/greginator/turbine-calculator.js
+++ js/greginator/turbine-calculator.js
@@ -501,14 +501,10 @@ onVersionChanged(function(version) {
 			}
 
 			var durability_time = 0;
-			// durability for non-plasma is 20% of energy output every 3000 ticks
-			var damage_taken = ((stats.energy_output*0.2)/3000);
-			if (selected_fuel.name.indexOf("Plasma") != -1) {
-				// durability for plasma is min(eu*0.2,eu^0.6)
-				damage_taken = Math.min(damage_taken,(stats.energy_output^0.6)/3000);
-			}
+			// durability for all turbines is min(output*0.2, output^0.6) every 1000 ticks
+			var damage_taken = Math.floor(Math.min(stats.energy_output*0.2,Math.pow(stats.energy_output,0.6))/1000*20); // multiply by 20 to convert to seconds instead of ticks
 
-			durability_time = selected_material[selected_size].durability / (damage_taken * 20); // divide by another 20 to get duration in seconds instead of ticks
+			durability_time = selected_material[selected_size].durability / damage_taken;
 			durability_time = formatTime(durability_time);
 
 			stats_container.empty();
