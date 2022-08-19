7ce23ab40af19683a483db14fe5544f431141a35
Divran <arviddivran@gmail.com>
2022-08-20 00:51:34 +0200

    fix

--- js/greginator/cell-calculator.js
+++ js/greginator/cell-calculator.js
@@ -126,7 +126,8 @@ onVersionChanged(function(version) {
 			// increment up in case any are below 1
 			var infloop = 10;
 			var original = output_mult;
-			var fractionConverter = [1,10,5,10,5,2,5,10,5,10];
+								//   0, 1,2, 3,  4,5,6, 7,8, 9
+			var fractionConverter = [1,10,5,10,2.5,2,5,10,5,10];
 			$(".cell-warning").hide();
 			do {
 				infloop--;
@@ -137,8 +138,13 @@ onVersionChanged(function(version) {
 				}
 				var inp = values.inputs.find(v => !v.is_item && ((v.recipe_amount * output_mult) % v.cell_size) != 0);
 				if (inp) {
-					var fraction = Math.floor((((inp.recipe_amount * output_mult) / inp.cell_size) % 1) * 10);
-					output_mult *= fractionConverter[fraction] || 1;
+					var amount = ((inp.recipe_amount * output_mult) / inp.cell_size);
+					if (amount < 1) {
+						output_mult *= 1/amount;
+					} else {
+						var fraction = Math.floor((amount % 1) * 10);
+						output_mult *= fractionConverter[fraction] || 1;
+					}
 					//console.log("change",fractionConverter[fraction] || 1);
 					//console.log("mult",output_mult);
 					//output_mult *= inp.cell_size / ((inp.recipe_amount * output_mult) % inp.cell_size);
@@ -146,8 +152,13 @@ onVersionChanged(function(version) {
 				}
 				var out = values.outputs.find(v => !v.is_item && ((v.recipe_amount * output_mult) % v.cell_size) != 0);
 				if (out) {
-					var fraction = Math.floor((((out.recipe_amount * output_mult) / out.cell_size) % 1) * 10);
-					output_mult *= fractionConverter[fraction] || 1;
+					var amount = ((out.recipe_amount * output_mult) / out.cell_size);
+					if (amount < 1) {
+						output_mult *= 1/amount;
+					} else {
+						var fraction = Math.floor((amount % 1) * 10);
+						output_mult *= fractionConverter[fraction] || 1;
+					}
 					//console.log("change",out.cell_size / ((out.recipe_amount * output_mult) % out.cell_size));
 					//console.log("mult",output_mult);
 					//output_mult *= out.cell_size / ((out.recipe_amount * output_mult) % out.cell_size);
